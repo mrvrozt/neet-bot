52525fcb465d97867a62f7bf269a7b944e106da5
mrvrozt <48779035+mrvrozt@users.noreply.github.com>
2019-04-13 03:51:32 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -87,12 +87,7 @@ client.on('message', message => {
 		message.channel.send( {files: ["./tsath/pinch.png"]});
 	}
 	
-	if (msg.includes(`BRAVO`)){
-		message.channel.send(":clap: :clap: :clap:");
-		message.channel.send( {files: ["./tsath/clap.png"]});
-	}
-	
-	if (msg.includes(`CONGRATS`)){
+	if (msg.includes(`CONGRATS`,`BRAVO`)){
 		message.channel.send(":clap: :clap: :clap:");
 		message.channel.send( {files: ["./tsath/clap.png"]});
 	}
