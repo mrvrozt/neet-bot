b675ac06e8260ccd0e610a8da9a5836831df0ad2
mrvrozt <48779035+mrvrozt@users.noreply.github.com>
2019-04-18 23:54:32 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ console.log(`Logged in as ${client.user.tag}!`);
     client.user.setPresence({
         game: {
             //name: '「東京放課後サモナーズ」',
-	    name: 'Pokémon Sword and Shield',
+	    name: 'Ueno Fluffy Dream Kingdom (Nightmare)',
             type: "STREAMING",
 	    //url: "https://www.twitch.tv/東京放課後サモナーズ"
             url: "https://www.twitch.tv/NeetFTW"
