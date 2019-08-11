9ec00dd06a705e035a5d2a34d4a084c14cecf845
mrvrozt <48779035+mrvrozt@users.noreply.github.com>
2019-08-11 21:21:47 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,7 +43,7 @@ console.log(`Logged in as ${client.user.tag}!`);
         game: {
             //name: '「東京放課後サモナーズ」',
 	    //name: 'Tokyo Afterschool Summoners',
-	    name: 'Seaside Fashionista',
+	    name: 'Embark! Summer Ocean Adventure',
 	    //name: 'Chunky/Burly/Hefty/Tubby/Beefy',
             type: "STREAMING",
 	    //type: "PLAYING",
